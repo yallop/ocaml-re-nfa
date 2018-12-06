fc8a35e089af73a6dc386df806cdc81e42ba4c03
Jeremy Yallop <yallop@gmail.com>
2018-12-06 09:43:36 +0000

    Add 'regenerate' tests

--- lib_test/tests.ml
+++ lib_test/tests.ml
@@ -1,3 +1,39 @@
+let regenerate_tests compile = 
+  let repeat i j re =
+    let r = ref Regex.eps in
+    for _ = 1 to i do r := Regex.seq !r re done;
+    (match j with
+    | None -> r := Regex.seq !r (Regex.star re)
+    | Some j -> for _ = succ i to j do r := Regex.seq !r (Regex.opt re) done);
+    !r
+  in
+  let alphabet = ['a'; 'b'; 'c'; 'd'; 'e'; 'f'] in
+  let allof cs = List.fold_right (fun c r -> Regex.(alt (chr c) r)) cs Regex.empty in
+  let noneof cs = allof (List.filter (fun c -> not (List.mem c cs)) alphabet) in
+
+  let rec to_re = let open Regenerate.Regex in function
+    | One -> Regex.eps
+    | Set (true, cs) -> allof cs
+    | Set (false, cs) -> noneof cs
+    | Seq (r1, r2) -> Regex.seq (to_re r1) (to_re r2)
+    | Or (r1, r2) -> Regex.alt (to_re r1) (to_re r2) 
+    | And _ | Not _ -> (* No support for intersection or complement at present *)
+       QCheck.assume_fail ()
+    | Rep (i,j,re) -> repeat i j (to_re re)
+  in
+  let check (re, pos, neg) =
+    let cre = compile (to_re re) in
+    List.for_all cre pos && not (List.exists cre neg)
+  in
+  let generator =
+    Regenerate.arbitrary
+      (module Regenerate.Word.String)
+      (module Segments.ThunkList(Regenerate.Word.String))
+      ~compl:false ~pp:Fmt.char ~samples:10 alphabet
+  in
+  ignore (QCheck_runner.run_tests [QCheck.Test.make generator check])
+
+
 let test compile =
   begin
     let a = compile "a" in
@@ -100,13 +136,21 @@ let explode s =
     if i < 0 then l else exp (i - 1) (s.[i] :: l) in
   exp (String.length s - 1) []
 
-let nfa_test s = 
-  let accept = Nfa.accept (Regex.compile (Regex.parse s)) in
-  fun s -> accept (explode s)
+let combinator_matcher ~compile ~accept =
+  fun r -> let c = compile r in fun s -> accept c s
+
+let string_matcher ~compile ~accept =
+  fun r -> combinator_matcher (Regex.parse r) ~compile ~accept 
+
+let nfa_accept r s = Nfa.accept r (explode s)
 
 let () =
   begin
     print_string "testing NFA acceptance..."; flush stdout;
-    test nfa_test;
+    test (string_matcher ~compile:Regex.compile ~accept:nfa_accept);
+    print_endline "OK!";
+
+    print_string "regenerate tests (NFA)..."; flush stdout;
+    regenerate_tests (combinator_matcher ~compile:Regex.compile ~accept:nfa_accept);
     print_endline "OK!";
   end
